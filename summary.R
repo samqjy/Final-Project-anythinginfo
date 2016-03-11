8c7596fc44fa61ae030e39684367d362c9b638f9
whereisirene <yl53@uw.edu>
2016-03-11 12:12:59 -0800

    separate the rscripts to functions

--- summary.R
+++ summary.R
@@ -3,37 +3,55 @@ library(dplyr)
 library(plotly)
 library(knitr)
 options(stringsAsFactors = FALSE)
+#tem_change <- read.csv("data/tem_change.csv")
 
 
-carbon <- read.csv("data/Carbon_Emission_Data.csv")
-tem_change <- read.csv("data/tem_change.csv")
-top_ten_emission <- tem_change %>% 
-  arrange(desc(tem_change)) %>%
-  head(n = 10) %>%
-  select(Country, tem_change, X1960, X2014)
+# Pass in the tem_change dataset and get the average temperature change
+get_avg <- function(tem_change) {
+  return(mean(tem_change$tem_change) %>%
+          round(digits=2))
+}
 
-top_ten_emission <- arrange(top_ten_emission, desc(tem_change))
-average_tem_change <- mean(tem_change$tem_change) %>%
-  round(digits=2)
-num_above_avg <- tem_change %>%
-  filter(tem_change < average_tem_change) %>%
-  ncol()
-colnames(top_ten_emission) <- c('Top 10 Carbon Emission Countries',  'Temperature Change since 1960',
-                                'Carbon Emission in 1960', 'Carbon Emission in 2014')
+#Pass in the tem_change dataset and get the number of countries above the average change
+get_num_above_avg <- function(tem_change) {
+  return(tem_change %>%
+          filter(tem_change < mean(tem_change$tem_change)) %>%
+           ncol())
+}
 
-l <- list(color = toRGB("grey"), width = 0.5)
+# Pass in the dataset of the temperature change and the function will return the top ten table
+get_top_ten <- function(tem_change) {
+  top_ten_emission <- tem_change %>% 
+    arrange(desc(tem_change)) %>%
+      head(n = 10) %>%
+        select(Country, tem_change, X1960, X2014)
+    
+  top_ten_emission <- arrange(top_ten_emission, desc(tem_change))
+  
+  colnames(top_ten_emission) <- c('Top 10 Carbon Emission Countries',  'Temperature Change since 1960',
+                                  'Carbon Emission in 1960', 'Carbon Emission in 2014')
+  kable(top_ten_emission)
+  
+}
 
-# specify map projection/options
-g <- list(
-  showframe = FALSE,
-  showcoastlines = FALSE,
-  projection = list(type = 'Mercator')
-)
+# Get a graph of the global temperature change
+get_graph <- function(tem_change) {
+  l <- list(color = toRGB("grey"), width = 0.5)
+  
+  
+  g <- list(
+    showframe = FALSE,
+    showcoastlines = FALSE,
+    projection = list(type = 'Mercator')
+  )
+  
+  plot_ly(tem_change, z = tem_change, text = Country, locations = Code, type = 'choropleth',
+          color = tem_change, colors = 'Oranges', marker = list(line = l),
+          colorbar = list(ticksuffix = '˚C', title = 'Global Temperature change since 1960')) %>%
+              layout(title = '2Global Temperature change since 1960<br>Source:<a href="http://berkeleyearth.lbl.gov/country-list/">Berkeley Earth</a>',
+                     geo = g)
+    
+}
 
-temp_graph <- plot_ly(tem_change, z = tem_change, text = Country, locations = Code, type = 'choropleth',
-        color = tem_change, colors = 'Oranges', marker = list(line = l),
-        colorbar = list(ticksuffix = '˚C', title = 'Global Temperature change since 1960')) %>%
-  layout(title = '2Global Temperature change since 1960<br>Source:<a href="http://berkeleyearth.lbl.gov/country-list/">Berkeley Earth</a>',
-         geo = g)
 
 
